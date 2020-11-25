460655e634b7e1b9b479816fabc2b1ad4ae84503
Zywczas <pzywczewski@opsenio.com>
2020-11-25 11:51:03 +0100

    Room done

--- app/src/main/java/com/zywczas/rickmorty/model/db/CharacterDao.kt
+++ app/src/main/java/com/zywczas/rickmorty/model/db/CharacterDao.kt
@@ -3,10 +3,12 @@ package com.zywczas.rickmorty.model.db
 import android.database.sqlite.SQLiteConstraintException
 import androidx.room.*
 import com.zywczas.rickmorty.model.Character
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CharacterDao {
 
+    //todo to chyba nie musi byc
     @Throws(SQLiteConstraintException::class)
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertCharacter(char : Character) : Long
@@ -14,7 +16,12 @@ interface CharacterDao {
     @Delete
     suspend fun deleteCharacter(char: Character) : Long
 
+    @Throws(Exception::class)
     @Query("SELECT * FROM characters")
-    suspend fun getCharacters() :
+    suspend fun getCharacters() : Flow<List<Character>>
+
+    @Throws(Exception::class)
+    @Query("SELECT COUNT(id) FROM characters WHERE id == :charId")
+    suspend fun getIdCount(charId: Int) : Flow<Int>
 
 }
