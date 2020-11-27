a053eff3919bc879a648febb21b8def8179203e7
Zywczas <pzywczewski@opsenio.com>
2020-11-27 08:24:07 +0100

    few todo' cleared

--- app/src/main/java/com/zywczas/rickmorty/utilities/ExtensionFunctions.kt
+++ app/src/main/java/com/zywczas/rickmorty/utilities/ExtensionFunctions.kt
@@ -11,10 +11,5 @@ private const val tag = "RickAndMorty"
 fun logD(msg : String) = Log.d(tag, msg)
 fun logD(e : Throwable) = Log.d(tag, "${e.message}")
 
-
-//todo dodac pagination w api fragment
-//todo w api musi byc diff util i podawana cala lista, zeby nie tracic danych przy obrocie
 //todo dodac pagination w DB module
-//todo prawdopodobnie zamienic Api modul na przesylanie tylko po 20 postaci bo w ApiFragment moge dodawac kolejne
 //todo dac wczytywanie zdjec z kamery
-//todo dodac diffutil do adaptera
